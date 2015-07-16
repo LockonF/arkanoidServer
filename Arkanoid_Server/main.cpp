a710a15eecb64e10b7124239fc220b49d73cf717
Daniel Franco <zurielovsko@gmail.com>
2015-07-15 20:13:04 -0500

    Actualizado para función asíncrona

--- Arkanoid_Server/main.cpp
+++ Arkanoid_Server/main.cpp
@@ -2,14 +2,11 @@
 #include <cstdlib>
 #include <iostream>
 
-
-
-
-
 int main(int argc, char* argv[])
 {
     try
     {
+        //inicializamos el servidor y lo dejamos correr en su propio thread
         boost::asio::io_service io_service;
         udp_server server(io_service,7200);
         io_service.run();
@@ -18,6 +15,6 @@ int main(int argc, char* argv[])
     {
         std::cerr << e.what() << std::endl;
     }
-        
-        return 0;
+    
+    return 0;
 }
--- Network/udp_server.cpp
+++ Network/udp_server.cpp
@@ -17,50 +17,33 @@ std::string make_string(const char * data)
         return std::string("not accepted");
 }
 
-//Funcion que se ejecuta asíncronamente una vez cada x tiempo
-
+//Funcion de prueba
 void udp_server::print()
 {
-    int count = participants_.size();
-    
-    
-    //Para cada participante le envía el string
-    for(auto participant: participants_)
+    for(auto participant : participants_)
     {
-        
-        //Este es un string de ejemplo, pero podemos enviar lo que sea
-        boost::shared_ptr<std::string> message(new std::string("hola"));
-        
-        
+        boost::shared_ptr<std::string> message(new std::string("Hola"));
         socket_.async_send_to(boost::asio::buffer(*message), participant,
                               boost::bind(&udp_server::handle_send, this, message,
                                           boost::asio::placeholders::error,
                                           boost::asio::placeholders::bytes_transferred));
-        
-    }
-    
 
-    
-    
-    //Timer, le agrega una cantidad de segundos segundos al temporizador
+    }
     timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
     timer_.async_wait(boost::bind(&udp_server::print, this));
+
 }
 
-
-//Incializamos el servidor con el puerto, el servicio io y el endpoint. Le decimos que asíncronamente puede ejecutar una función cada n tiempo sin dejar de lado la recepción.
+//Incializamos el servidor con el puerto, el servicio io y el endpoint
 udp_server::udp_server(boost::asio::io_service &io_service, int port): socket_(io_service, udp::endpoint(udp::v4(), port)), timer_(io_service, boost::posix_time::seconds(1))
 {
     timer_.async_wait(boost::bind(&udp_server::print, this));
-
     start_receive();
 }
 
 
 //Función para recibir
 void udp_server::start_receive(){
-    
-    
     socket_.async_receive_from(
                                boost::asio::buffer(recv_buffer_), remote_endpoint_,
                                boost::bind(&udp_server::handle_receive, this,
@@ -74,9 +57,9 @@ void udp_server::handle_receive(const boost::system::error_code &error, std::siz
 {
     if (!error || error == boost::asio::error::message_size)
     {
-        //Guardamos el remote endpoint, insert se asegura de que no haya repetidos
-        participants_.insert(remote_endpoint_);
         
+        //Registramos el endpoint como participante
+        participants_.insert(remote_endpoint_);
         
         //Mandamos a llamar a la función que fabrica el string
         boost::shared_ptr<std::string> message(new std::string(make_string(recv_buffer_.data())));
